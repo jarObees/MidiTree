bc609b7156afb352031c8056a95b8fbe9483ad76
Jose Gomez <josegomezstuffs@gmail.com>
2025-03-03 22:25:29 -0600

    Added ground colors definitions

--- Source/Colors.h
+++ Source/Colors.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <jive_core/jive_core.h>
+
+namespace jiveUI::colors
+{
+	static const auto ground_primary = jive::parseColour("#7A594F");
+	static const auto ground_secondary = jive::parseColour("#C09173");
+	static const auto ground_tertiary = jive::parseColour("#DFB386");
+	static const auto ground_highlight = jive::parseColour("#FFF0C7");
+}

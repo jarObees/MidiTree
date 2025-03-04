f76adf4bb8548c1e60d2378916cd80bb892d320c
Jose Gomez <josegomezstuffs@gmail.com>
2025-03-03 22:53:38 -0600

    Add: more colors!

--- Source/Colors.h
+++ Source/Colors.h
@@ -7,4 +7,10 @@ namespace jiveUI::colors
 	static const auto ground_secondary = jive::parseColour("#C09173");
 	static const auto ground_tertiary = jive::parseColour("#DFB386");
 	static const auto ground_highlight = jive::parseColour("#FFF0C7");
+	
+	static const auto sky_primary = jive::parseColour("#81A3B7");
+
+	static const auto crown_primary = jive::parseColour("#5F7055");
+	static const auto crown_secondary = jive::parseColour("#97946B");
+	static const auto crown_highlight = jive::parseColour("#AEA151");
 }
